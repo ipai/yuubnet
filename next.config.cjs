35c11918909d1acb4b6ed20d06bd414bbe720eb8
Ita Pai <ipai.mc@gmail.com>
2025-02-12 20:41:00 -0800

    Versioning untracked files

--- next.config.cjs
+++ next.config.cjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Required for Cloudflare Pages
+  skipTrailingSlashRedirect: true,
+  skipMiddlewareUrlNormalize: true,
   images: {
     remotePatterns: [
       {
