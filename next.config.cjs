709a78073e3e280a70fb2e3252e9c94dd0fa2b52
Ita Pai <ipai.mc@gmail.com>
2025-02-12 21:50:34 -0800

    fix build

--- next.config.cjs
+++ next.config.cjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Required for Cloudflare Pages
-  skipTrailingSlashRedirect: true,
-  skipMiddlewareUrlNormalize: true,
   images: {
     remotePatterns: [
       {
